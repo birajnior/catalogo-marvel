b6caf5e788c93a6b47c6169ec3b1bcdebe9746eb
birajnior <ubiratanbernardo@gmail.com>
2022-09-15 20:24:52 -0300

    Subindo Códigos.

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+
+
+
+// for (let i = 0; i < listaFilmes.length; i++) {
+//     document.write('<img src=' + listaFilmes[i] + '>')
+
+// }
+
+// img0.href = "https://alura.com.br/"
+// img0.src= listaFilmes[0]
+
+// window.open("https://youtu.be/-RkXHIEWq0w", "_blank")
+
